ab41c36581e890b8acbc63d7ae60dd54f4177679
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-19 21:29:58 -0400

    commit tree fd99df4bacbb15c025f1a4a15fcbc6e143c01761

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home");
+
+function wrapAdjective(action="a hard worker"){
+    const encouragingPromptFunction = wrapAdjective('*')
+    return `You are ${action}`;
+}
+
+// function wrapAdjective(action="*"){
+//      const emphatic = "a hard worker"
+//     return function(single="special") {
+//         const wrapped = "|| a dedicated worker ||"
+//           return `You are ${action}${emphatic}${action}!` 
+
+//     }
+//   }
+
+function wrapAdjective(flare="*"){
+    return function(def="special"){
+        return `You are ${flare}${def}${flare}!`
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
+
+
+
+
+    
+//     let encouragingPromptFunction = wrapAdjective(emphatic="special") {
+//         return `You are ${wrap}${emphatic}${wrap}!`
+//     }
+// }
+
+// function wrapAdjective() {
+//     let result = wrapAdjective(emphatic = '*a hard worker*');
+//     return result(`You are ${emphatic}!`);    
+// }
+
+
+
+// || `*${emphatic}*`;
